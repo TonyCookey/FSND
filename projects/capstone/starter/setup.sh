3872a6ad13ab6769efb8202a2e53c40ab4a22bb1
Tony Cookey <tonyiroegbu59@gmail.com>
2020-08-02 22:26:42 +0100

    FIX: updated setup.sh with new variable

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -2,3 +2,10 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 export FLASK_DEBUG=True
+export AUTH0_DOMAIN='tonycookey.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='coffeeapp'
+export SQLALCHEMY_DATABASE_URI='postgresql://postgres:cookey07@localhost:5432/castingAgency'
+export SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
